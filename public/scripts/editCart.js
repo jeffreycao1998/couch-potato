b8eb58d1e5b5387e65fed01396927d1ae9e3dbb4
jeffreycao1998 <jeffreycao1998@hotmail.com>
2020-08-23 11:05:02 -0400

    users can remove items from cart

--- public/scripts/editCart.js
+++ public/scripts/editCart.js
@@ -1,3 +1,13 @@
 const letUserEditCart = () => {
-  
+  $('.remove-item').off('click');
+
+  $('.remove-item').on('click', (event) => {
+    const cart = JSON.parse(Cookies.get('cart'));
+    const itemId = event.target.className.split(' ')[1].slice(7);
+
+    delete cart[itemId];
+    Cookies.set('cart', JSON.stringify(cart));
+    renderCartItems();
+    getCartTotalPrice();
+  });
 };
--- public/scripts/renderCartItems.js
+++ public/scripts/renderCartItems.js
@@ -2,23 +2,25 @@ const renderCartItems = () => {
   const cart = JSON.parse(Cookies.get('cart'));
   let template = ``;
 
-  for (let item in cart) {
+  for (let itemId in cart) {
     template += `
-    <div id="${cart[item].name}-cart" class="cart-item-container">
+    <div id="${cart[itemId].name}-cart" class="cart-item-container">
       <div class="cart-item">
-        <h5 class="cart-item-label">${cart[item].name.replace(/-/g,' ')}</h5>
-        <h5 class="cart-item-cost">$${cart[item].price / 100}</h5>
+        <h5 class="cart-item-label">${cart[itemId].name.replace(/-/g,' ')}</h5>
+        <h5 class="cart-item-cost">$${cart[itemId].price / 100}</h5>
       </div>
       <div class="cart-item-buttons">
-        <ion-icon name="close-circle"></ion-icon>
+        <ion-icon class="remove-item remove-${itemId}" name="close-circle"></ion-icon>
         <div class="adjust-quantity">
-          <ion-icon name="remove-circle-outline"></ion-icon>
-          <div class="item-quantity">${cart[item].quantity}</div>
-          <ion-icon name="add-circle-outline"></ion-icon>
+          <ion-icon class="increment-item increment-${itemId}" name="remove-circle-outline"></ion-icon>
+          <div class="item-quantity">${cart[itemId].quantity}</div>
+          <ion-icon class="decrement-item decrement-${itemId}" name="add-circle-outline"></ion-icon>
         </div>
       </div>
     </div>`;
   }
 
   $('.cart-items').html(template);
+
+  letUserEditCart();
 };
