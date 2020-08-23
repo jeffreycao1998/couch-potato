b3a1c2ea74ca17912999227bf7d95ab9d6517f4c
Camilo Gomez <camilogomezmusic@gmail.com>
2020-08-23 02:46:35 +0000

    Update database, consolidate apiRoutes"

--- db/seeds/02_menu_items.sql
+++ db/seeds/02_menu_items.sql
@@ -5,20 +5,20 @@ INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES
 INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES ('White potatoes', 1, 199, 'https://farmtofamily.co.il/wp-content/uploads/2020/08/white-potatoes.jpg', 'Enjoy delicious russet potatoes directly from local farms');
 
 
-INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES ('Seasoned baked potatoes', 2, 799, 'https://www.pexels.com/photo/baked-potatoes-with-rosemary-garnish-162763/', 'Delicious white potatoes seasoned with our garlic rosemary house seasoning, baked to perfection');
+INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES ('Seasoned baked potatoes', 2, 799, 'https://images.pexels.com/photos/162763/delicious-garnish-potatoes-fried-162763.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260', 'Delicious white potatoes seasoned with our garlic rosemary house seasoning, baked to perfection');
 
 
-INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES ('Baked potato wedges', 2,  899, 'https://www.pexels.com/photo/photo-of-vegetables-on-bowl-3297363/', 'Baked potato wedges seasoned with our delicious paprika and chili seasoning');
+INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES ('Baked potato wedges', 2,  899, 'https://images.pexels.com/photos/1439177/pexels-photo-1439177.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 'Baked potato wedges seasoned with our delicious paprika and chili seasoning');
 
 
-INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES ('Baked sweet potato cubes', 2,  899, 'https://www.pexels.com/photo/sliced-orange-vegetable-on-white-paper-3233282/', 'Baked sweet potato cubes seasoned with italian spices, deliciously chewy');
+INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES ('Baked sweet potato cubes', 2,  899, 'https://images.pexels.com/photos/3233282/pexels-photo-3233282.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 'Baked sweet potato cubes seasoned with italian spices, deliciously chewy');
 
 INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES ('Vegan sweet potato shepherds pie', 2,  899, 'https://www.fitfoodienutter.com/wp-content/uploads/2018/01/Vegan-shepherds-pie-15.jpg', 'Our own twist on an all time family favorite');
 
 
-INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES  ('House fries', 3, 699, 'https://www.pexels.com/photo/fried-potatoes-1583884/', 'You can''t go wrong with an all time classic');
+INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES  ('House fries', 3, 699, 'https://images.pexels.com/photos/1583884/pexels-photo-1583884.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'You can''t go wrong with an all time classic');
 
-INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES ('Sweet potato fries', 3, 849, 'https://www.pexels.com/photo/fries-and-sauce-dip-1893555/', 'Delicious thin sliced sweet potato fries with our signature bbq sauce');
+INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES ('Sweet potato fries', 3, 849, 'https://images.pexels.com/photos/1893555/pexels-photo-1893555.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 'Delicious thin sliced sweet potato fries with our signature bbq sauce');
 
 
 INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES  ('Irish potato cakes', 4,  899, 'https://tarasmulticulturaltable.com/wp-content/uploads/2014/03/irish-potato-cakes-5-of-6.jpg', 'Delicious pan fried cheesy potato cakes');
@@ -27,4 +27,4 @@ INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES
 INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES  ('Crispy mashed potato pancakes', 4, 899, 'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_1460,h_1825/k%2FPhoto%2FRecipes%2F2019-11-recipe-leftover-mashed-potato-pancakes%2F2019-10-21_Kitchn88899_Thanksgiving-Leftover-Pancakes', 'The only thing better than dinner is breakfast for dinner!');
 
 
-INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES  ('Sweet potato brownie', 5, 899, 'https://www.pexels.com/photo/chocolate-with-milted-chocolate-on-white-ceramic-plate-45202/', 'We know what you''re thinking, but it is not too good to be true');
+INSERT INTO menu_items (name, category_id, price, photo_url, description) VALUES  ('Sweet potato brownie', 5, 899, 'https://www.sweetcannela.com/wp-content/uploads/2015/06/SPB2-1024x1024.jpg', 'We know what you''re thinking, but it is not too good to be true');
