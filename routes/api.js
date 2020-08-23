63b47cdd6aa7ded77c640d2a40474e2f1e1b1c58
jeffreycao1998 <jeffreycao1998@hotmail.com>
2020-08-23 05:50:30 -0400

    pulled changes

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const { getDistanceFromLocation } = require('../serverFunctions/axios');
+const { getDistanceFromLocation } = require('../server_scripts/axios');
 
 module.exports = (db) => {
   router.get("/distanceFromStore", (req, res) => {
